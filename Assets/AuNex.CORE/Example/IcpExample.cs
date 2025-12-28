045d3ae0cfbaad87d0b5ae70ae3629f66525fe68
motii8128 <default270robo@gmail.com>
2025-12-28 23:48:38 +0900

    [fix] 座標系をUnityに合わせる

--- Assets/AuNex.CORE/Example/IcpExample.cs
+++ Assets/AuNex.CORE/Example/IcpExample.cs
@@ -86,7 +86,7 @@ public class IcpExample : MonoBehaviour
         }
 
         // オドメトリのゲームオブジェクトを更新（デバッグ用）
-        odometry.transform.position = new Vector3((float)translation_.y, 0.25f, (float)translation_.x);
+        odometry.transform.position = new Vector3((float)translation_.x, 0.25f, (float)translation_.y);
         odometry.transform.rotation = MathUtils.ToQuatUnity(rotation_);
     }
 
