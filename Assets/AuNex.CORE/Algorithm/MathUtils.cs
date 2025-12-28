9f432c332e42cbd50af5973d2d3575b4a9f2c448
motii8128 <default270robo@gmail.com>
2025-12-28 21:34:33 +0900

    [new] ２次元回転行列とクォータニオンの相互変換を追加

---
+++ Assets/AuNex.CORE/Algorithm/MathUtils.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using MathNet.Numerics.LinearAlgebra;
+using UnityEngine;
+
+
+namespace AuNex
+{
+    namespace Algorithm
+    {
+        public class MathUtils
+        {
+            /// <summary>
+            /// 回転行列をQuaternionに変換する
+            /// </summary>
+            /// <param name="rot_matrix">回転行列(２次元)</param>
+            /// <returns>Quaternion</returns>
+            public static Quaternion ToQuat(Matrix<double> rot_matrix)
+            {
+                float angle = Mathf.Atan2((float)rot_matrix[1, 0], (float)rot_matrix[0, 0]);
+                return Quaternion.Euler(0, 0, angle);
+            }
+
+            /// <summary>
+            /// Quaternionを回転行列に変換する
+            /// </summary>
+            /// <param name="quat">Quaternion</param>
+            /// <returns>回転行列(2x2)</returns>
+            public static Matrix<double> ToRotMatrix(Quaternion quat)
+            {
+                float angle = quat.eulerAngles.z * Mathf.Deg2Rad;
+                Matrix<double> rot_matrix = Matrix<double>.Build.Dense(2, 2);
+                rot_matrix[0, 0] = Mathf.Cos(angle);
+                rot_matrix[0, 1] = -Mathf.Sin(angle);
+                rot_matrix[1, 0] = Mathf.Sin(angle);
+                rot_matrix[1, 1] = Mathf.Cos(angle);
+                return rot_matrix;
+            }
+        }
+    }
+}
