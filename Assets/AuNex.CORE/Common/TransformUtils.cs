75a346cfc00ddd58b9de9c36adbcda92a5b18114
motii8128 <default270robo@gmail.com>
2025-12-31 11:55:09 +0900

    [new] 座標変換ライブラリを追加

---
+++ Assets/AuNex.CORE/Common/TransformUtils.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ROS2;
+
+namespace AuNex
+{
+    namespace Common
+    {
+        public static class TransformUtils
+        {
+            /// <summary>
+            /// ROS2座標をUnity座標に変換
+            /// </summary>
+            /// <param name="vector3"></param>
+            /// <returns></returns>
+            public static Vector3 Ros2Unity(this Vector3 vector3)
+            {
+                return new Vector3(-vector3.y, vector3.z, vector3.x);
+            }
+
+            /// <summary>
+            /// Unity座標をROS2座標に変換
+            /// </summary>
+            /// <param name="vector3"></param>
+            /// <returns></returns>
+            public static Vector3 Unity2Ros(this Vector3 vector3)
+            {
+                return new Vector3(vector3.z, -vector3.x, vector3.y);
+            }
+
+            /// <summary>
+            /// ROS2姿勢をUnity姿勢に変換
+            /// </summary>
+            /// <param name="quaternion"></param>
+            /// <returns></returns>
+            public static Quaternion Ros2Unity(this Quaternion quaternion)
+            {
+                return new Quaternion(quaternion.y, -quaternion.z, -quaternion.x, quaternion.w);
+            }
+
+            /// <summary>
+            /// Unity姿勢をROS2姿勢に変換
+            /// </summary>
+            /// <param name="quaternion"></param>
+            /// <returns></returns>
+            public static Quaternion Unity2Ros(this Quaternion quaternion)
+            {
+                return new Quaternion(-quaternion.z, quaternion.x, -quaternion.y, quaternion.w);
+            }
+
+            public static tf2_msgs.msg.TFMessage CreateTFMessage(geometry_msgs.msg.TransformStamped transform)
+            {
+                var tf = new tf2_msgs.msg.TFMessage();
+
+                tf.Transforms = new[]{transform};
+
+                return tf;
+            }            
+        }
+    }// end of namespace Common
+}// end of namespace AuNex
+
+
