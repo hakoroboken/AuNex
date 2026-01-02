ff74d0268eedf41679ec95b236d1ecf3f4d1501a
motii8128 <default270robo@gmail.com>
2026-01-02 21:42:32 +0900

    [update] 速度調整に自由度をもたせた。

--- Assets/AuNex.CORE/Control/PurePursuit.cs
+++ Assets/AuNex.CORE/Control/PurePursuit.cs
@@ -11,42 +11,62 @@ namespace AuNex
         public class PurePursuit
         {
             private float lookAheadDistance = 0.0f;
+            private int targetIdx;
+            private nav_msgs.msg.Path path_;
+            private float velocityGain = 0.0f;
 
-            public PurePursuit(float look_ahead_distance)
+            public PurePursuit(float look_ahead_distance, float velocity_gain)
             {
                 lookAheadDistance = look_ahead_distance;
+                velocityGain = velocity_gain;
             }
 
-            public geometry_msgs.msg.Twist Compute(geometry_msgs.msg.PoseStamped current_pose, nav_msgs.msg.Path path)
+            public void SetPath(nav_msgs.msg.Path path)
+            {
+                path_ = path;
+                targetIdx = 0;
+            }
+
+            public geometry_msgs.msg.Twist Compute(geometry_msgs.msg.PoseStamped current_pose)
             {
                 var target_pose = new geometry_msgs.msg.PoseStamped();
-                bool found = false;
-                foreach(var p in path.Poses)
+                
+                for(int i = targetIdx; i < path_.Poses.Length; i++)
                 {
+                    var p = path_.Poses[i];
+
                     double _dx = p.Pose.Position.X - current_pose.Pose.Position.X;
                     double _dy = p.Pose.Position.Y - current_pose.Pose.Position.Y;
 
                     if((_dx*_dx+_dy*_dy) > lookAheadDistance*lookAheadDistance)
                     {
-                        target_pose = p;
-                        found = true;
                         break;
                     }
+
+                    targetIdx++;
                 }
 
-                if(!found)target_pose = path.Poses[path.Poses.Length-1];
+                if(targetIdx > path_.Poses.Length-1)targetIdx = path_.Poses.Length-1;
+
+                target_pose = path_.Poses[targetIdx];
 
                 double dx = target_pose.Pose.Position.X - current_pose.Pose.Position.X;
                 double dy = target_pose.Pose.Position.Y - current_pose.Pose.Position.Y;
-                double distance = Math.Sqrt(dx*dx + dy*dy);
+                
+                double distance = velocityGain * Math.Sqrt(dx*dx + dy*dy);
 
-                double alpha = Math.Atan2(dy, dx) - TransformUtils.QuatToYaw(current_pose.Pose.Orientation) - 90.0 * (double)Mathf.Deg2Rad;
+                double alpha = Math.Atan2(dy, dx) - TransformUtils.QuatToYaw(current_pose.Pose.Orientation)- 90.0f*Mathf.Deg2Rad;
 
                 var cmd_vel = new geometry_msgs.msg.Twist();
                 cmd_vel.Linear.Y = distance;
-                cmd_vel.Angular.Z = 2.0 * distance * Math.Sin(alpha) / lookAheadDistance;
+                cmd_vel.Angular.Z = distance * (2.0 * Math.Sin(alpha) / lookAheadDistance);
 
                 return cmd_vel;
+            }
+
+            public int GetTargetIdx()
+            {
+                return targetIdx;
             }
         }
     }
